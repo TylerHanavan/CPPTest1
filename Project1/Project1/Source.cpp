b4841e37aac795743eac26eee1599543e7d283a1
Tyler Hanavan <TylerHanavan@gmail.com>
2016-11-12 12:11:33 -0500

    Added functions

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+int sum(int a, int b) {
+	return a + b;
+}
+
+string combo(int a, int b) {
+	return to_string(a) + to_string(b);
+}
+
 int main() {
 
-	cout << "Hello World";
+	cout << "Hello World" << endl << sum(5, 6) << endl << combo(5, 6);
 
 	std::cin.ignore();
 	return 0;
