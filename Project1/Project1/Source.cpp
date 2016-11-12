d40e65269c26e2c83771e3148384a884c9a9f8d2
Tyler Hanavan <TylerHanavan@gmail.com>
2016-11-12 12:32:34 -0500

    Experimenting with arrays

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -15,6 +15,14 @@ string combo(int a, int b) {
 
 int main() {
 
+	int ints[7] = { 1, 2, 3, 5, 2, 1, 3 };
+
+	for (int i : ints) {
+		cout << i << endl;
+	}
+
+	cout << "Number 3 of ints is: " << ints[3] << endl;
+
 	cout << "Hello World" << endl << sum(5, 6) << endl << combo(5, 6);
 
 	std::cin.ignore();
